faf50cabf6ead52579e2fc9c86b29581546f482c
6-LL <lorenzanilautaro+github@gmail.com>
2026-03-19 09:32:40 -0300

    initial commit

---
+++ src/client/java/io/github/lau6l/totem_of_allying/client/TotemOfAllyingClient.java
@@ -0,0 +1,10 @@
+package io.github.lau6l.totem_of_allying.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TotemOfAllyingClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/io/github/lau6l/totem_of_allying/TotemOfAllying.java
@@ -0,0 +1,10 @@
+package io.github.lau6l.totem_of_allying;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TotemOfAllying implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
