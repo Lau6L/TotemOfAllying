6c185d76098b0590e4fe20d90af24240b0942f24
6-LL <lorenzanilautaro+github@gmail.com>
2026-03-19 10:02:26 -0300

    main class logger, component initialization

--- src/main/java/io/github/lau6l/totem_of_allying/TotemOfAllying.java
+++ src/main/java/io/github/lau6l/totem_of_allying/TotemOfAllying.java
@@ -1,10 +1,18 @@
 package io.github.lau6l.totem_of_allying;
 
+import io.github.lau6l.totem_of_allying.component.Components;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TotemOfAllying implements ModInitializer {
+    public static final String MOD_ID = "totem_of_allying";
+    public static final Logger LOGGER = LoggerFactory.getLogger("Totem of Allying");
 
     @Override
     public void onInitialize() {
+        LOGGER.info("Initializing Totem of Allying...");
+
+        Components.registerComponents();
     }
 }
---
+++ src/main/java/io/github/lau6l/totem_of_allying/component/Components.java
@@ -0,0 +1,20 @@
+package io.github.lau6l.totem_of_allying.component;
+
+import net.minecraft.component.ComponentType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+
+import java.util.function.UnaryOperator;
+
+public class Components {
+    public static final ComponentType<AlliedEntityComponent> ALLIED_ENTITY_COMPONENT = register(
+            "allied_entity", builder -> builder.codec(AlliedEntityComponent.CODEC).packetCodec(AlliedEntityComponent.PACKET_CODEC).cache()
+    );
+
+    private static <T> ComponentType<T> register(String id, UnaryOperator<ComponentType.Builder<T>> builderOperator) {
+        return Registry.register(Registries.DATA_COMPONENT_TYPE, id, builderOperator.apply(ComponentType.builder()).build());
+    }
+
+    public static void registerComponents() {
+    }
+}
