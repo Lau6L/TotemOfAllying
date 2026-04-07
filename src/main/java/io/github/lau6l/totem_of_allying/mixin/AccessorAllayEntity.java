5c374e33d5ccca436f796f8389c7fea05d1e5bd3
6-LL <lorenzanilautaro+github@gmail.com>
2026-04-07 15:08:13 -0300

    multiple version building setup added

--- src/main/java/io/github/lau6l/totem_of_allying/mixin/AccessorAllayEntity.java
+++ src/main/java/io/github/lau6l/totem_of_allying/mixin/AccessorAllayEntity.java
@@ -2,7 +2,7 @@ package io.github.lau6l.totem_of_allying.mixin;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.AllayEntity;
-import org.jspecify.annotations.Nullable;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
